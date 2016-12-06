94c9dac749ec45ecf3a59e1975da6aab6917dcdf
Evandro Xavero <evandromelos@hotmail.com>
2016-12-05 23:52:25 -0200

    Primeiros fontes

---
+++ app/main.js
@@ -0,0 +1,16 @@
+var Nav = React.createClass({
+    render: function () {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container">
+
+                </div>
+            </nav>
+        );
+    }
+});
+
+ReactDOM.render(
+    <h1> Hello React </h1>,
+    document.getElementById('app')
+);
