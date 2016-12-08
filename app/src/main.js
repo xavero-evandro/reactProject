cedcb19f98f38fbef7f74c631fe6e2e66face7b3
Evandro Xavero <evandromelos@hotmail.com>
2016-12-07 23:17:37 -0200

    aula 8

--- app/src/main.js
+++ app/src/main.js
@@ -1,53 +1,3 @@
-var Nav = React.createClass({
-    render: function () {
-        return (
-            <nav className="navbar navbar-default">
-                <div className="container">
-                    <div className="navbar-header">
-                        <a href={ this.props.linkUrl } className="navbar-brand">
-                            { this.props.title }
-                        </a>
-                    </div>
-                </div>
-            </nav>
-        );
-    }
-});
-
-var Title = React.createClass({
-    render: function () {
-        return (
-            <div className="row">
-                <h1>{ this.props.title }</h1>
-            </div>
-        );
-    }
-});
-
-var Button = React.createClass({
-
-    getInitialState: function () {
-        return {
-            click: false
-        };
-    },
-
-    toogleClick: function () {
-        this.setState({
-            click: !this.state.click
-        });
-    },
-
-    render: function () {
-        var btnClass = this.state.click ? 'btn btn-warning' : 'btn btn-success';
-        var title = this.state.click ? this.props.textActive : this.props.title;
-        return (
-            <button onClick={ this.toogleClick } className={btnClass}>{title}</button>
-        );
-    }
-});
-
-
 var Page = React.createClass({
     render: function () {
         return (
@@ -56,7 +6,8 @@ var Page = React.createClass({
                 <div className="container">
                     <Title title="My Component Title"/>
                     <div className="row">
-                        < Button title="My Button" textActive="Loading..."/>
+                        <Form/>
+                        < Button title="Send" textActive="Loading..."/>
                     </div>
                 </div>
             </myElement>
